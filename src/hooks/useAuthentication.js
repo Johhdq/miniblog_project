9528f13c9a07578b5f4b9717c7f52ed6b9869a39
João Augusto Druzian <johhdq@hotmail.com>
2022-12-07 12:06:09 -0300

    adicionado hook de autenticação

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,29 @@
+// funções que vão vir do firebase
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
+
+import { useState, useEffect } from "react";
+
+export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // para não correr o risco de funções ficarem executando
+  // cleanup
+  // deal with memory leak
+
+  const [cancelled, setCancelled] = useState(false);
+
+  const auth = getAuth();
+
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return;
+    }
+  }
+};
