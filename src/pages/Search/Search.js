9983f1a2bb11af2683e29ae3a333ff15e71d0fe1
João Augusto Druzian <johhdq@hotmail.com>
2023-01-11 17:04:19 -0300

    adições

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,17 +1,34 @@
 // hooks
+import { Link } from "react-router-dom";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 // novo hook apenas para resgatar o valor da url, isso porque depois pode-se utilizar ele em outros locais também
 import { useQuery } from "../../hooks/useQuery";
 
+// components
+import { PostDetails } from "../../components/PostDetails";
+
 export const Search = () => {
   const query = useQuery();
   const search = query.get("q");
 
+  const { documents: posts } = useFetchDocuments("posts", search);
+
   return (
     <div>
       <h2>Search</h2>
-      <p>{search}</p>
+      <div>
+        {posts && posts.length === 0 && (
+          <>
+            <p>Não foram encontrados posts através da sua busca...</p>
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </>
+        )}
+        {posts &&
+          posts.map((post) => <PostDetails key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
